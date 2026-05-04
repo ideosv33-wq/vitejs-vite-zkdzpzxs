5bc9f2bb211b4779aa24e5b03f883b77540669e6
ideosv33-wq <ideosv33@gmail.com>
2026-05-04 15:38:25 -0300

    Updated types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+
+
 // declaracion de tipos de datos que van a llegar, lo pongo aca en este archivo porque asi lo puedo usar en varios lados importandolo, para no escribir varias veces
 
 export interface iRestaurante {
